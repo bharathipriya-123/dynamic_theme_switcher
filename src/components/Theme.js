8359f52f11348ae0d1c6fa13983e60d163fd4acf
bharathi20 <bharathipriya20112000@gmail.com>
2024-12-19 21:19:21 +0530

    second commit

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -57,12 +57,60 @@
 //     );
 // }
 
-import React from 'react';
-import { useState } from 'react';
-import '../style/Theme.css'
+// import React from 'react';
+// import { useState } from 'react';
+// import '../style/Theme.css'
+// import { useEffect } from 'react';
+
+// export default function Theme() {
+//     // const [themeMode, setThemeMode] = useState(false);
+//     const [themeMode, setThemeMode] = useState(() => {
+//         const savedTheme = localStorage.getItem('themeMode');
+//         return savedTheme === 'true'; 
+//     });
+
+//     useEffect(() => {
+//         localStorage.setItem('themeMode', themeMode);
+//     }, [themeMode]);
+
+//     const switcher = () => {
+//         setThemeMode((prevMode) => !prevMode);
+//     };
+
+//     const buttonText = themeMode ? "Switch to Dark Mode" : "Switch to Light Mode";
+
+//     return (
+//         <div className={`theme-container ${themeMode ? "light-theme" : "dark-theme"}`}>
+//             <h2>Dynamic Theme Switcher</h2>
+//             <h1>{themeMode ? "Light Mode" : "Dark Mode"}</h1>
+//             <img
+//                 src="https://pbs.twimg.com/media/GS1Nc_nXAAA4aKh?format=jpg&name=large"
+//                 alt="Theme"
+//                 className="theme-image"
+//             />
+//             <button className="theme-button" onClick={switcher}>
+//                 {buttonText}
+//             </button>
+//         </div>
+//     );
+       
+        
+// }
+
+import React, { useState, useEffect } from 'react';
+import '../style/Theme.css';
 
 export default function Theme() {
-    const [themeMode, setThemeMode] = useState(false);
+   
+    const [themeMode, setThemeMode] = useState(() => {
+        const savedTheme = localStorage.getItem('themeMode');
+        return savedTheme === 'true'; 
+    });
+
+   
+    useEffect(() => {
+        localStorage.setItem('themeMode', themeMode);
+    }, [themeMode]);
 
     const switcher = () => {
         setThemeMode((prevMode) => !prevMode);
@@ -84,6 +132,4 @@ export default function Theme() {
             </button>
         </div>
     );
-       
-        
 }
